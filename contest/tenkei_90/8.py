537b52100a2eeb35c922b1a690239611067a5141
jamuojisan <kage0427@gmail.com>
2022-08-28 10:25:49 +0900

    耳dp

---
+++ contest/tenkei_90/8.py
@@ -0,0 +1,20 @@
+n = int(input())
+s = input()
+def add(a, b):
+    a += b
+    a %= mod
+    return a
+dp = [[0]*(8) for _ in range(n+1)]
+usemoji = 'atcoder'
+mod =10**9 +7
+dp[0][0] = 1
+
+for i in range(1,n+1):
+    moji = s[i-1]
+    for j in range(8):
+        dp[i][j]  = add(dp[i][j], dp[i-1][j])
+        if j>0 and moji == usemoji[j-1]:
+            dp[i][j] = add(dp[i][j], dp[i-1][j-1])
+print(dp[n][7])     
+
+
