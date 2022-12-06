7a7632f5a497b1bd3fd57a6a9dc6eb3cb358b80a
jamuojisan <kage0427@gmail.com>
2022-12-06 21:27:55 +0900

    solved

--- tessoku/B/13.py
+++ tessoku/B/13.py
@@ -1,2 +1,16 @@
-N, K = map(int, input().split())
-A = list(map(int, input().split()))
+N ,K = map(int, input().split())
+A = [*map(int, input().split())]
+
+right = 0
+ans = 0
+val = 0
+for left in range(N):
+    while(right < N and val + A[right]<= K ):
+        val += A[right]
+        right += 1
+    ans += right - left
+    if right == left:
+        right +=1
+    else:
+        val -= A[left]
+print(ans)
