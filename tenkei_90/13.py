9935a45c47a3d0897536a1f4da4052bb6458f8bc
jamuojisan <kage0427@gmail.com>
2022-12-24 13:02:09 +0900

    [minor]

---
+++ tenkei_90/13.py
@@ -0,0 +1,33 @@
+import heapq
+def diekstra(G,a):
+    q = []
+    heapq.heappush(q, (0,a))
+    visited = [-1 for _ in range(N)]
+    done = [False for _ in range(N)]
+    visited[a] = 0
+    while(len(q) > 0):
+        d, v= heapq.heappop(q)
+        
+        if done[v] :
+            continue
+        done[v] = True
+        for (c, nv) in G[v]:
+            if visited[nv] == -1 or visited[nv] > visited[v] + c:
+                visited[nv] = visited[v] + c
+                heapq.heappush(q, (visited[nv],nv))
+    return visited
+N, M = map(int, input().split())
+G = [[] for _ in range(N)]
+# input Graph
+for _ in range(M):
+    a, b,c = map(int, input().split())
+    a, b = a-1, b-1
+    G[a].append([c,b])
+    G[b].append([c,a])
+
+# dis[1,k] + dis[k, N] = dis[1,k] +dis[N,k]に気を付ければ、解ける
+dis1 = diekstra(G,0)
+dis2 = diekstra(G,N-1)
+
+for i in range(N):
+    print(dis1[i] + dis2[i])
