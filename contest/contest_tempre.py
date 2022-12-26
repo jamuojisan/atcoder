8ffa738a56078556fb3b5ed6b6b7d04d97e8a418
jamuojisan <kage0427@gmail.com>
2022-12-26 20:26:29 +0900

    solved

--- contest/contest_tempre.py
+++ contest/contest_tempre.py
@@ -26,24 +26,5 @@ def judge(val):
             return False
     return True
 
-N, M = map(int, input().split())
-D = [int(input()) for _ in range(N)]
-C = [int(input()) for _ in range(M)]
 
-dp = [[10**9]*(M+1) for _ in range(N+1)]
-for i in range(M+1):
-    dp[0][i] = 0
-
-for n in range(1, N+1):
-    dis = D[n-1]
-    for m in range(M+1):
-        if n > m:
-            continue
-        c = C[m-1]
-        dp[n][m] = min(dp[n][m], dp[n-1][m-1] + c*dis, dp[n][m-1])
-ans = 10 ** 9
-for i in range(M+1):
-    ans = min(ans, dp[N][i])      
-
-print(ans)
-
+K, A, B = map(int, input().split())
