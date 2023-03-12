e61476dbffbcfe7abc2ae58421c4d0894b5eec68
jamuojisan <kage0427@gmail.com>
2023-03-12 17:58:46 +0900

    add

--- contest/library/seisu.py
+++ contest/library/seisu.py
@@ -79,3 +79,27 @@ def ncr(n,r, m):
     bunbo = bunbo1*bunbo2 % m
     inv = modinv(bunbo,m)
     return bunsi * inv % m
+
+# 繰り返し二乗法(行列)
+def mat_mul(a, b, m):
+    I, J, K = len(a), len(b[0]), len(b)
+    c = [[0] * J for _ in range(I)]
+    for i in range(I):
+        for j in range(J):
+            for k in range(K):
+                c[i][j] += a[i][k] * b[k][j]
+            c[i][j] %= m
+    return c
+
+def mat_pow(x, n, m):
+    y = [[0]*len(x) for _ in range(len(x))]
+    
+    for i in range(len(x)):
+        y[i][i] = 1
+    
+    while(n > 0):
+        if n&1:
+            y = mat_mul(x,y, m)
+        x = mat_mul(x,x, m)
+        n >>=1
+    return y
